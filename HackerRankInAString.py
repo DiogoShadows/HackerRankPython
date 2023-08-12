5f81464536c2ea60f5b09888fa2df701b0b4ff01
Diogo Santos <diogo_f_s_@hotmail.com>
2023-08-12 11:10:05 -0300

    HackerRank in a String

---
+++ HackerRankInAString.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hackerrankInString' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def hackerrankInString(s):
+    stringHackerRank = "hackerrank"
+    indexString = 0
+
+    for letter in s:
+        if letter == stringHackerRank[indexString]:
+            indexString += 1
+
+        if indexString > 9:
+            return "YES"
+        
+    return "NO"
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = hackerrankInString(s)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
