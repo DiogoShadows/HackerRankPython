883548b0566e42457d35095636cf92d2bb096c36
Diogo Santos <diogo_f_s_@hotmail.com>
2023-08-13 19:14:08 -0300

    Pangrams

---
+++ Pangrams.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    # Write your code here
+    totalUnique = 0
+    for letter in set(s.lower()):
+        if letter.isalpha():
+            totalUnique += 1
+
+    return "pangram" if totalUnique == 26 else "not pangram"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
